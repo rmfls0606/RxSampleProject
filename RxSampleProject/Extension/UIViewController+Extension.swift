febaa776ec98e58875018d92d4c0acab656298e1
rmfls0606 <rmfls0606@naver.com>
2025-08-20 01:49:08 +0900

    feat: UIViewController를 확장하여 Alert 띄우는 로직 생성

---
+++ RxSampleProject/Extension/UIViewController+Extension.swift
@@ -0,0 +1,33 @@
+//
+//  UIViewController+Extension.swift
+//  RxSampleProject
+//
+//  Created by 이상민 on 8/20/25.
+//
+
+import UIKit
+
+extension UIViewController {
+    func showAlert(
+        title: String,
+        message: String,
+        okHandler: @escaping () -> Void
+    ){
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        
+        let ok = UIAlertAction(title: "확인", style: .default){ _ in
+            okHandler()
+        }
+        
+        let cancel = UIAlertAction(title: "취소", style: .cancel)
+        
+        alert.addAction(ok)
+        alert.addAction(cancel)
+        
+        present(alert, animated: true)
+    }
+}
