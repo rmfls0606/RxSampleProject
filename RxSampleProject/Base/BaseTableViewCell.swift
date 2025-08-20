42c1b4d481ae29920199d00325e3b625ea4a03fe
rmfls0606 <rmfls0606@naver.com>
2025-08-20 20:46:50 +0900

    feat: UITableViewCell 공통 구조를 위한 BaseTableViewCell 생성

---
+++ RxSampleProject/Base/BaseTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  BaseTableViewCell.swift
+//  RxSampleProject
+//
+//  Created by 이상민 on 8/20/25.
+//
+
+import UIKit
+
+class BaseTableViewCell: UITableViewCell {
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureHierarchy(){}
+    func configureLayout(){}
+    func configureView(){}
+}
