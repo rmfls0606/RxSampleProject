6263c9e074ab57c9ae7413500613291c5c057f94
rmfls0606 <rmfls0606@naver.com>
2025-08-20 20:45:54 +0900

    feat: 추천 이름을 위한 UserCollectionView UI 구성 및 AutoLayout 설정

---
+++ RxSampleProject/Homework/View/UserCollectionViewCell.swift
@@ -0,0 +1,36 @@
+//
+//  UserCollectionViewCell.swift
+//  iOSAcademy-RxSwift
+//
+//  Created by Jack on 1/30/25.
+//
+
+import UIKit
+import SnapKit
+
+final class UserCollectionViewCell: BaseCollectionViewCell {
+    
+    let label: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 13)
+        return label
+    }()
+    
+    override func configureHierarchy() {
+        contentView.addSubview(label)
+    }
+    
+    override func configureLayout() {
+        label.snp.makeConstraints { make in
+            make.edges.equalTo(contentView)
+        }
+    }
+    
+    override func configureView() {
+        layer.cornerRadius = 8
+        layer.borderWidth = 1
+        layer.borderColor = UIColor.black.cgColor
+    }
+}
+
