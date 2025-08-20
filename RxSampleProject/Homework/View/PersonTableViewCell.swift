09eb7c3b70182069297c2ca80e82dc6de3a978b6
rmfls0606 <rmfls0606@naver.com>
2025-08-20 20:48:16 +0900

    feat: 사용자 목록을 보여줄 PersonTableViewCell 생성

---
+++ RxSampleProject/Homework/View/PersonTableViewCell.swift
@@ -0,0 +1,70 @@
+//
+//  UserTableViewCell.swift
+//  iOSAcademy-RxSwift
+//
+//  Created by Jack on 1/30/25.
+//
+
+import UIKit
+import SnapKit
+
+final class PersonTableViewCell: BaseTableViewCell {
+    
+    let usernameLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 18, weight: .bold)
+        label.textColor = .black
+        return label
+    }()
+    
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        imageView.backgroundColor = .systemMint
+        imageView.layer.cornerRadius = 8
+        return imageView
+    }()
+    
+    let detailButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("받기", for: .normal)
+        button.setTitleColor(.systemBlue, for: .normal)
+        button.isUserInteractionEnabled = true
+        button.backgroundColor = .lightGray
+        button.layer.cornerRadius = 16
+        return button
+    }()
+    
+    override func configureHierarchy() {
+        contentView.addSubview(usernameLabel)
+        contentView.addSubview(profileImageView)
+        contentView.addSubview(detailButton)
+    }
+    
+    override func configureLayout() {
+        profileImageView.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+            $0.leading.equalTo(20)
+            $0.size.equalTo(60)
+        }
+        
+        usernameLabel.snp.makeConstraints {
+            $0.centerY.equalTo(profileImageView)
+            $0.leading.equalTo(profileImageView.snp.trailing).offset(8)
+            $0.trailing.equalTo(detailButton.snp.leading).offset(-8)
+        }
+        
+        detailButton.snp.makeConstraints {
+            $0.centerY.equalTo(profileImageView)
+            $0.trailing.equalToSuperview().inset(20)
+            $0.height.equalTo(32)
+            $0.width.equalTo(72)
+        }
+    }
+     
+    override func configureView() {
+        self.selectionStyle = .none
+    }
+}
+
