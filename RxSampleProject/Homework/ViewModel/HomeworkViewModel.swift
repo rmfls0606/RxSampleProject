04639deba798430793d0205d9a5e465d92ff66b3
rmfls0606 <rmfls0606@naver.com>
2025-08-21 23:47:45 +0900

    feat: HomeworkViewModel Input/Output 구조로 파일 생성

---
+++ RxSampleProject/Homework/ViewModel/HomeworkViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  HomeworkViewModel.swift
+//  RxSampleProject
+//
+//  Created by 이상민 on 8/21/25.
+//
+
+import Foundation
+
+final class HomeworkViewModel {
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    func transform(input: Input) -> Output {
+        return Output()
+    }
+}
