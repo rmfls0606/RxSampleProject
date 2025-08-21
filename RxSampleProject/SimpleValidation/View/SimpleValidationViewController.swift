74d02b7b8a6d15a9d8b7320be4a767da6ec220c2
rmfls0606 <rmfls0606@naver.com>
2025-08-21 23:42:58 +0900

    feat: SimpleValidationViewController를 MVVM + Input/Output을 통해 로직 구현

--- RxSampleProject/SimpleValidation/View/SimpleValidationViewController.swift
+++ RxSampleProject/SimpleValidation/View/SimpleValidationViewController.swift
@@ -10,11 +10,12 @@ import SnapKit
 import RxSwift
 import RxCocoa
 
-private let minimalUsernameLength = 5
-private let minimalPasswordLength = 5
+
 
 final class SimpleValidationViewController: BaseViewController {
     //MARK: - Property
+    private let viewModel = SimpleValidationViewModel()
+    
     private let disposeBag = DisposeBag()
     
     //MARK: - View
@@ -30,7 +31,7 @@ final class SimpleValidationViewController: BaseViewController {
         let textField = UITextField()
         textField.borderStyle = .roundedRect
         textField.font = .systemFont(ofSize: 17)
-        textField.placeholder = "Username has to be at least \(minimalUsernameLength) characters"
+//        textField.placeholder = "Username has to be at least \(minimalUsernameLength) characters"
         return textField
     }()
     
@@ -52,14 +53,14 @@ final class SimpleValidationViewController: BaseViewController {
         let textField = UITextField()
         textField.borderStyle = .roundedRect
         textField.font = .systemFont(ofSize: 17)
-        textField.placeholder = "Password has to be at least \(minimalPasswordLength) characters"
+//        textField.placeholder = "Password has to be at least \(minimalPasswordLength) characters"
         return textField
     }()
     
     private let passwordValidationLabel: UILabel = {
         let label = UILabel()
         label.textColor = .red
-        label.text = "\(minimalPasswordLength)자 이상 입력해주세요."
+//        label.text = "\(minimalPasswordLength)자 이상 입력해주세요."
         label.font = .systemFont(ofSize: 12)
         return label
     }()
@@ -127,52 +128,50 @@ final class SimpleValidationViewController: BaseViewController {
     
     override func configureView() {
         view.backgroundColor = .white
+    }
+    
+    override func configureBind() {
+        let input = SimpleValidationViewModel.Input(
+            usernameValid: usernameTextField.rx.text.orEmpty,
+            passwordValid: passwordTextField.rx.text.orEmpty,
+            successButtonTapped: successButton.rx.tap
+        )
         
-        let usernameValid = usernameTextField.rx.text.orEmpty
-            .map{ $0.count >= minimalUsernameLength }
-            .share(replay: 1)
+        let output = viewModel.transform(input: input)
         
-        let passwordValid = passwordTextField.rx.text.orEmpty
-            .map{ $0.count >= minimalPasswordLength }
-            .share(replay: 1)
-        
-        let everyhingValid = Observable.combineLatest(usernameValid, passwordValid)
-            .map{ $0 && $1}
-            .share(replay: 1)
-        
-        usernameValid
+        output.usernameValid
             .bind(to: passwordTextField.rx.isEnabled)
             .disposed(by: disposeBag)
         
-        usernameValid
+        output.usernameValid
             .bind(with: self, onNext: { owner, valid in
                 if valid{
                     owner.usernameValidationLabel.textColor = .green
                     owner.usernameValidationLabel.text = "정상적으로 입력되었습니다."
                 }else{
                     owner.usernameValidationLabel.textColor = .red
-                    owner.usernameValidationLabel.text = "\(minimalUsernameLength)자 이상 입력해주세요."
+                    owner.usernameValidationLabel.text = "\(owner.viewModel.minimalUsernameLength)자 이상 입력해주세요."
                 }
             })
             .disposed(by: disposeBag)
         
-        passwordValid
-            .bind(with: self, onNext: { owner, valid in
+        output.passwordValid
+            .bind(with: self) { owner, valid in
                 if valid{
                     owner.passwordValidationLabel.textColor = .green
                     owner.passwordValidationLabel.text = "정상적으로 입력되었습니다."
                 }else{
                     owner.passwordValidationLabel.textColor = .red
-                    owner.passwordValidationLabel.text = "\(minimalPasswordLength)자 이상 입력해주세요."
+                    owner.passwordValidationLabel.text = "\(owner.viewModel.minimalPasswordLength)자 이상 입력해주세요."
                 }
-            })
+            }
             .disposed(by: disposeBag)
         
-        everyhingValid
+        output.everyhingValid
             .bind(to: successButton.rx.isEnabled)
             .disposed(by: disposeBag)
         
-        successButton.rx.tap
+        output.submit
             .bind(with: self) { owner, value in
                 owner.showAlert(title: "RxExample", message: "This is wonderful") {
                     
--- RxSampleProject/SimpleValidation/ViewModel/SimpleValidationViewModel.swift
+++ RxSampleProject/SimpleValidation/ViewModel/SimpleValidationViewModel.swift
@@ -6,18 +6,55 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
 final class SimpleValidationViewModel {
+    private(set) var minimalUsernameLength = 5
+    private(set) var minimalPasswordLength = 5
     
     struct Input {
-        
+        let usernameValid: ControlProperty<String>
+        let passwordValid: ControlProperty<String>
+        let successButtonTapped: ControlEvent<Void>
     }
     
     struct Output {
-        
+        let usernameValid: Observable<Bool>
+        let passwordValid: Observable<Bool>
+        let everyhingValid: Observable<Bool>
+        let submit: Observable<Void>
     }
     
     func transform(input: Input) -> Output {
-        return Output()
+        let usernameValid = input.usernameValid
+            .withUnretained(self)
+            .map({ owner, username in
+                username.count >= owner.minimalUsernameLength
+            })
+            .share(replay: 1)
+            
+        let passwordValid = input.passwordValid
+            .withUnretained(self)
+            .map({ owner, password in
+                password.count >= owner.minimalPasswordLength
+            })
+            .share(replay: 1)
+        
+        let everyhingValid = Observable.combineLatest(usernameValid, passwordValid)
+            .map{ $0 && $1}
+            .share(replay: 1)
+        
+        let submit = input.successButtonTapped
+            .withLatestFrom(everyhingValid)
+            .filter { $0 }
+            .map { _ in () }
+            
+        
+        return Output(usernameValid: usernameValid,
+                      passwordValid: passwordValid,
+                      everyhingValid: everyhingValid,
+                      submit: submit
+        )
     }
 }
