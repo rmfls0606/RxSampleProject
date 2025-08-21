08a331b36324a9ea10c28f158b90ae1542784ea3
rmfls0606 <rmfls0606@naver.com>
2025-08-21 21:33:14 +0900

    feat: SimpleValidationViewModel Input/Output 구조로 파일 생성

---
+++ RxSampleProject/SimpleValidation/ViewModel/SimpleValidationViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  SimpleValidationViewModel.swift
+//  RxSampleProject
+//
+//  Created by 이상민 on 8/21/25.
+//
+
+import Foundation
+
+final class SimpleValidationViewModel {
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    func transform(input: Input) -> Output {
+        return Output()
+    }
+}
