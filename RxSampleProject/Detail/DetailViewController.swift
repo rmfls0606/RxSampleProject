caa93d530546de30338045b99544b196cc73a93b
rmfls0606 <rmfls0606@naver.com>
2025-08-22 01:33:19 +0900

    feat: DetailViewController 파일 생성

---
+++ RxSampleProject/Detail/DetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.swift
+//  RxSampleProject
+//
+//  Created by 이상민 on 8/22/25.
+//
+
+import UIKit
+
+final class DetailViewController: BaseViewController {
+    override func configureView() {
+        view.backgroundColor = .white
+        
+        navigationItem.title = "Detail"
+    }
+}
